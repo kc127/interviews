38ca56081e1a01eeb5b2044b389d53a297b48472
Kanchan Chauhan <kanchan827@gmail.com>
2022-02-05 11:05:11 -0500

    o(n) solution with set

--- palindrome/longestPalindrome.js
+++ palindrome/longestPalindrome.js
@@ -1,3 +1,27 @@
+/*
+Given a string s which consists of lowercase or uppercase letters, return the length of the longest palindrome that can be built with those letters.
+
+Letters are case sensitive, for example, "Aa" is not considered a palindrome here.
+
+
+
+Example 1:
+
+Input: s = "abccccdd"
+Output: 7
+Explanation:
+One longest palindrome that can be built is "dccaccd", whose length is 7.
+
+
+Intuition:
+  - check how many chars have odd freq using a set and a evenCount variable
+  - after first traversal,
+      if set length is 0 then we don't have any chars with odd val
+      - which means return evenCount * 2
+      else
+        return evenCount * 2 + 1
+      */
+
 /**
  * @param {string} s
  * @return {number}
