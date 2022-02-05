7e607c0091c3983a181ebd55ea2d810e88f4bd1d
Kanchan Chauhan <kanchan827@gmail.com>
2022-02-05 11:01:43 -0500

    palindrome permutation intuition

---
+++ palindrome/palindromePermutation.js
@@ -0,0 +1,15 @@
+/*
+Given a string s, return true if a permutation of the string could form a palindrome.
+
+Example 1:
+
+Input: s = "code"
+Output: false
+
+intuition:
+  - only a palindromic string can have permutation of it
+  - check if the curr string is a palindrome (oddlen vs evenlen)
+  - if yes, return true
+  - else return false
+*/
+
